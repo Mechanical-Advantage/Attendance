b8622c41942fd5d7c7a9bff4b5427567762b0b42
jaw99 <jonahwbonner@gmail.com>
2019-03-18 20:44:26 -0400

    Added rotation of server log to nightly refresh

--- nightlyRefresh.sh
+++ nightlyRefresh.sh
@@ -1,4 +1,10 @@
+#Cycle probemon log
 cd '/home/jaw99/python/probemon'
 date=`date '+%m-%d-%Y at %I:%M:%S %p'`
 cp 'probemon.log' "../../Attendance_data/logs/probemon/$date.log"
 echo '' > 'probemon.log'
+
+#Cycle server log
+cd '/home/jaw99/Attendance_data/logs'
+cp 'serverlog.log' "./serverlogs/$date.log"
+echo '' > 'serverlog.log'
