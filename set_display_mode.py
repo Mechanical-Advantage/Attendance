cde5fd7273f27d71685b15778fccfbd878f57a76
Jonah <47046556+jonahb55@users.noreply.github.com>
2019-09-06 18:35:52 -0400

    Add command line functionality

--- set_display_mode.py
+++ set_display_mode.py
@@ -1,10 +1,15 @@
 import sqlite3 as sql
+import sys
 
-rawInput = input("Display pure slideshow? (y-n) ")
-if rawInput == "y":
-    toWrite = 1
+if len(sys.argv) > 1:
+    toWrite = sys.argv[1]
 else:
-    toWrite = 0
+    rawInput = input("Display pure slideshow? (y-n) ")
+    if rawInput == "y":
+        toWrite = 1
+    else:
+        toWrite = 0
+    
 conn = sql.connect("/home/attendance/Attendance_data/attendance.db")
 conn.cursor().execute("UPDATE slideshowSettings SET pureSlideshow=?", (toWrite,))
 conn.commit()
