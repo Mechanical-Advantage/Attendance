e903450aab9401a3a1270d9aa21eaf336f50b04c
Jonah <47046556+jonahb55@users.noreply.github.com>
2019-09-06 18:39:00 -0400

    Add reading of slack URL from file

--- slack.py
+++ slack.py
@@ -1,5 +1,5 @@
 import requests
 
-webhook_url = "https://hooks.slack.com/services/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"
+webhook_url = open("/home/attendance/Attendance_data/slack_url.txt", "r").read()
 def post(message):
 	requests.post(webhook_url, json={"text": message})
