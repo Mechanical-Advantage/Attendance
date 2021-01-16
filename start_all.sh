73b300bc4497be56c46c54e04b7a3cf344fb494f
Jonah Bonner <jonahwbonner@gmail.com>
2021-01-16 15:41:14 -0500

    Add 30 second delay to ngrok connection

--- start_all.sh
+++ start_all.sh
@@ -8,7 +8,8 @@ sudo iptables -t nat -A PREROUTING -p tcp --dport 80 -j REDIRECT --to-port "$WEB
 sudo iptables -t nat -I OUTPUT -p tcp -d 127.0.0.1 --dport 80 -j REDIRECT --to-port "$WEB_PORT"
 exo-open --launch TerminalEmulator "sudo python3.8 $REPO_PATH/monitor.py"
 exo-open --launch TerminalEmulator "python3.8 $REPO_PATH/web_server.py"
+firefox -url 'http://127.0.0.1/manual' &
 if [ "$ENABLE" = true ]; then
+    sleep 30
     exo-open --launch TerminalEmulator "ngrok tcp 22"
 fi
-firefox -url 'http://127.0.0.1/manual' &
