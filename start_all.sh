224a8948ee9a942c1f2d21026aa409f348f25149
Jonah Bonner <jonahwbonner@gmail.com>
2021-01-19 12:30:19 -0500

    Remove 30 second wait from ngrok start

--- start_all.sh
+++ start_all.sh
@@ -8,8 +8,7 @@ sudo iptables -t nat -A PREROUTING -p tcp --dport 80 -j REDIRECT --to-port "$WEB
 sudo iptables -t nat -I OUTPUT -p tcp -d 127.0.0.1 --dport 80 -j REDIRECT --to-port "$WEB_PORT"
 exo-open --launch TerminalEmulator "sudo python3.8 $REPO_PATH/monitor.py"
 exo-open --launch TerminalEmulator "python3.8 $REPO_PATH/web_server.py"
-firefox -url 'http://127.0.0.1/manual' &
 if [ "$ENABLE_NGROK" = true ]; then
-    sleep 30
     exo-open --launch TerminalEmulator "ngrok tcp 22"
 fi
+firefox -url 'http://127.0.0.1/manual' &
