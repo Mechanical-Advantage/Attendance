3993280d15db757f9135ad088b9a1d9e8d11f227
Jonah Bonner <jonahwbonner@gmail.com>
2021-02-04 15:21:42 -0500

    Update config

--- config.py
+++ config.py
@@ -11,21 +11,21 @@ signin_title = "6328 Sign In/Out"
 mon_write_wait = 10 # secs, how long to wait between db writes when monitoring
 mon_nolog = ["00:17:3f:84:7f:bf", "78:92:9c:fd:17:d1", "00:20:a6:f6:90:98", "94:10:3e:8c:35:4b", "00:0c:43:38:83:70"] # which macs to skip logging (router, wifi adapters, etc.)
 mon_interfaces = {
-	# "INTERN": {
-	# 	"standard": "wlp2s0"
-	# },
-	"BELKIN": {
-		"standard": "wlx00173f847fbf"
+	"INTERN": {
+		"standard": "wlp2s0"
 	},
+	# "BELKIN": {
+	# 	"standard": "wlx00173f847fbf"
+	# },
 	"PROXIM": {
 		"standard": "wlx0020a6f69098"
 	}
 }
 mon_network_enable = True
 mon_ip_range = ("192.168.1.0", "192.168.1.255") # range of IP addresses to scan
-mon_ping_wait = 30 # seconds, how long to wait between ping cycles
+mon_ping_wait = 0 # seconds, how long to wait between ping cycles
 mon_ping_timeout = 500 # ms, timeout for network pings
-mon_success_wait = 300 # seconds, how long to wait after a successful ping before retrying
+mon_success_wait = 60 # seconds, how long to wait after a successful ping before retrying
 
 # Analysis
 recordkeeper_times = { # in minutes
